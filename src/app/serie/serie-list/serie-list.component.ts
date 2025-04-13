11ff62acb4e3c208e0a8e02f394e86c5a8217414
Ma Alejandra Carrillo <ma.carrillo2@uniandes.edu.co>
2025-04-13 13:55:24 -0500

    Cambios Finales Tarjeta

--- src/app/serie/serie-list/serie-list.component.ts
+++ src/app/serie/serie-list/serie-list.component.ts
@@ -10,14 +10,39 @@ import { SerieService } from '../serie.service';
 })
 export class SerieListComponent implements OnInit {
   series: Array<Serie> = [];
+  promedioTemporadas: number = 0;
+  nombreSerie: string = '';
+  descripcionSerie: string = '';
+  urlSerie: string = '';
+  imagenSerie: string = '';
+  visibilidad: boolean = false;
+
   constructor(private serieService: SerieService) { }
+
   getSeries() {
     this.serieService.getSeries().subscribe(data => {
       this.series = data;
     });
   }
+  mostrarSerie(serie:Serie){
+    this.nombreSerie = serie.name;
+    this.descripcionSerie = serie.description;
+    this.urlSerie = serie.webpage;
+    this.imagenSerie = serie.poster;
+    this.visibilidad = true;
+  }
+
+  calcularPromedioTemporadas(){
+    let total = 0;
+    this.series.forEach(serie => {
+      total += serie.seasons;
+    });
+    return total / this.series.length;
+  }
+
   ngOnInit() {
     this.getSeries();
+    this.promedioTemporadas = this.calcularPromedioTemporadas();
   }
 
 }
